bd325a95faebc549f157d7d815a95e1d2688bb89
HappyFeetMM <82187341+HappyFeetMM@users.noreply.github.com>
2021-04-09 01:42:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#Copyright (c) 2021 HappyFeetMM
+#All rights reserved.
+
+#This source code is licensed under the BSD-style license found in the
+#LICENSE file in the root directory of this source tree. 
 #!/usr/bin/env python
 # coding: utf-8
 
